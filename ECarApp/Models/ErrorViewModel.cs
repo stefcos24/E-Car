f1e059f8e4d55e4662f87ef9049dcd6d59caee49
stefcos24 <stefan22bl@gmail.com>
2022-02-01 19:51:12 +0100

    Add project files.

---
+++ ECarApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ECarApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
